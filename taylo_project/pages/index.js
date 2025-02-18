8ed6824a28958a603d87805ee3f80993f33620d5
Taylo2025 <arielmateos@gmail.com>
2025-02-17 21:55:39 -0300

    Add files via upload

---
+++ taylo_project/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+        return (
+            <div style={{ textAlign: "center", marginTop: "50px" }}>
+                <h1>¡Bienvenido a Taylo!</h1>
+                <p>Tu plataforma de préstamos personales.</p>
+            </div>
+        );
+    }
